4d058c7c68e66b61be3b8102673e06fad01d5503
Алексей <32929527+nub24@users.noreply.github.com>
2023-06-06 19:06:39 +0700

    install mongoose and connect to db

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const { PORT = 3000 } = process.env;
 
 const app = express();
 
+mongoose
+  .connect('mongodb://localhost:27017/mestodb')
+  .then(() => console.log('DB ok'))
+  .catch((err) => console.log('DB error', err))
+
+
 app.listen(PORT, () => {
-  console.log(`Сервер запущен на порту ${PORT}`)
+  console.log(`Server ok`)
 })
