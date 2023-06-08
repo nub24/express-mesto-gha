16902ba1411cc2110d36aaae234bb428f2fcdc9c
Алексей <32929527+nub24@users.noreply.github.com>
2023-06-08 09:18:42 +0700

    autotests fix_1

--- controllers/users.js
+++ controllers/users.js
@@ -18,7 +18,11 @@ module.exports.getUsers = (req, res) => {
 
 module.exports.getUserById = (req, res) => {
   User.findById(req.params._id)
-    .then(user => res.send({ data: user }))
+    .then(user => {
+      if (!user) {
+        res.status(404).send( {message: 'Пользователь не найден'})
+      }
+      return res.send({ data: user })})
     .catch(() => res.status(400).send({ message: 'Произошла ошибка' }));
 }
 
