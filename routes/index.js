37f78c3be893759b5027966fdc55373751b47d73
Алексей <32929527+nub24@users.noreply.github.com>
2023-06-11 00:24:23 +0700

    router index

---
+++ routes/index.js
@@ -0,0 +1,13 @@
+const router = require('express').Router();
+
+const userRouter = require('./user');
+const cardRouter = require('./card');
+const { ERROR_NOT_FOUND } = require('../utils/constants');
+
+router.use('users', userRouter);
+router.use('card', cardRouter);
+router.use((req, res) => {
+  res.status(ERROR_NOT_FOUND).send({ message: 'Страница не найдена' });
+});
+
+module.exports = router;
