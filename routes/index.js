d2b26a2372ba6cb037542695bf5a7b9bff2bb13c
Алексей <32929527+nub24@users.noreply.github.com>
2023-06-11 00:27:15 +0700

    router index-1

--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,8 @@ const userRouter = require('./user');
 const cardRouter = require('./card');
 const { ERROR_NOT_FOUND } = require('../utils/constants');
 
-router.use('users', userRouter);
-router.use('card', cardRouter);
+router.use('/users', userRouter);
+router.use('/card', cardRouter);
 router.use((req, res) => {
   res.status(ERROR_NOT_FOUND).send({ message: 'Страница не найдена' });
 });
