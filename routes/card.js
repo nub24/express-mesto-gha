ead3893386cb2216999de5b73b4c90dca2abbcc9
nub24 <nub24@rambler.ru>
2023-06-21 01:14:20 +0700

    validation getbyId-7

--- routes/card.js
+++ routes/card.js
@@ -1,4 +1,5 @@
 const cardRouter = require('express').Router();
+const { validationCreateCard } = require('../middlewares/validation');
 const {
   createCard,
   getCards,
@@ -8,7 +9,7 @@ const {
 } = require('../controllers/card');
 
 cardRouter.get('/', getCards);
-cardRouter.post('/', createCard);
+cardRouter.post('/', validationCreateCard, createCard);
 cardRouter.delete('/:_id', deleteCard);
 cardRouter.put('/:_id/likes', likeCard);
 cardRouter.delete('/:_id/likes', dislikeCard);
