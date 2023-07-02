edfa49e29717a455999b78d84aa9a23d514ca307
nub24 <nub24@rambler.ru>
2023-07-02 14:11:48 +0700

    log and cors

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const { errors } = require('celebrate');
+const cors = require('cors');
 const router = require('./routes/index');
 const errorHandler = require('./middlewares/errorHandler');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 const { PORT = 3000 } = process.env;
 const app = express();
@@ -12,10 +14,17 @@ mongoose
   .then(() => console.log('DB ok'))
   .catch((err) => console.log(`DB error: ${err}`));
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// Логирование запросов
+app.use(requestLogger);
+
 app.use('/', router);
+
+// Логирование ошибок
+app.use(errorLogger);
 
 app.use(errors());
 app.use(errorHandler);
